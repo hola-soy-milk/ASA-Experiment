61a9fb04dfce158cac172dab46c32521942b527d
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-06-16 19:21:13 +0200

    Add Second button, label, and adjust positions

--- app/layouts/tone_test_layout.rb
+++ app/layouts/tone_test_layout.rb
@@ -2,27 +2,49 @@ class ToneTestLayout < MotionKit::Layout
 
 	view :one_sound_button
 	view :two_sound_button
+	view :play_button
 
 	def layout
 		background_color UIColor.whiteColor
+		@label = add UILabel, :question_label
 		@one_sound_button = add UIButton.buttonWithType(UIButtonTypeSystem), :one_sound_button
 		@two_sound_button = add UIButton.buttonWithType(UIButtonTypeSystem), :two_sound_button
+		@play_button = add UIButton.buttonWithType(UIButtonTypeSystem), :play_button
+		@label.textAlignment = NSTextAlignmentCenter
 	end
 
 	def one_sound_button_style
 
-    title 'One'
-		font UIFont.systemFontOfSize(25)
+    title '1'
+		font UIFont.systemFontOfSize(24)
 		height '25'
-		width '50'
+		width '100'
     center ['25%', '75% + 50']
   end
 
 	def two_sound_button_style
-		title 'Two'
-		font UIFont.systemFontOfSize(25)
+		title '2'
+		font UIFont.systemFontOfSize(24)
 		height '25'
-		width '50'
+		width '100'
 		center ['75%', '75% + 50']
 	end
+
+	def play_button_style
+		title 'Play'
+		height '48'
+		width '100% - 10'
+		font UIFont.systemFontOfSize(48)
+		center ['50%', '40%']
+	end
+
+	def question_label_style
+		text 'How many tones can you hear?'
+		text_color UIColor.blackColor
+		font UIFont.systemFontOfSize(18)
+		height '24'
+		width '100%'
+		center ['50%', '60%']
+	end
+
 end
