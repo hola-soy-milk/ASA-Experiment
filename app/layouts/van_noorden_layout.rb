ed3f0849f9b710ce290934b09e5c5cb7baed2123
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-06-18 22:29:33 +0200

    Add unsure button

--- app/layouts/van_noorden_layout.rb
+++ app/layouts/van_noorden_layout.rb
@@ -2,6 +2,7 @@ class VanNoordenLayout < MotionKit::Layout
 
 	view :one_sound_button
 	view :two_sound_button
+	view :cant_say_sound_button
 	view :play_button
 
 	def layout
@@ -9,24 +10,32 @@ class VanNoordenLayout < MotionKit::Layout
 		@label = add UILabel, :question_label
 		@one_sound_button = add UIButton.buttonWithType(UIButtonTypeSystem), :one_sound_button
 		@two_sound_button = add UIButton.buttonWithType(UIButtonTypeSystem), :two_sound_button
+		@cant_say_sound_button = add UIButton.buttonWithType(UIButtonTypeSystem), :cant_say_sound_button
 		@play_button = add UIButton.buttonWithType(UIButtonTypeSystem), :play_button
 	end
 
 	def one_sound_button_style
-
-    title '1'
+    title 'One'
 		font UIFont.systemFontOfSize(24)
 		height '25'
 		width '100'
-    center ['25%', '100%']
+    center ['0% + 54', '100%']
   end
 
 	def two_sound_button_style
-		title '2'
+		title 'Two'
 		font UIFont.systemFontOfSize(24)
 		height '25'
 		width '100'
-		center ['75%', '100%']
+		center ['33% + 54', '100%']
+	end
+
+	def cant_say_sound_button_style
+		title "Unsure"
+		font UIFont.systemFontOfSize(24)
+		height '25'
+		width '100'
+		center ['66% + 54', '100%']
 	end
 
 	def play_button_style
