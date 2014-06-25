b53fe5ad740414eb622d0c50cf0b4076a6560371
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-06-25 11:58:26 +0200

    Add test subject info controller

---
+++ app/controllers/test_subject_info_controller.rb
@@ -0,0 +1,27 @@
+class TestSubjectInfoController < UIViewController
+	def loadView
+		@layout = TestSubjectInfoLayout.new
+		self.view =  @layout.view
+
+		@id_field = @layout.id_field
+		@alcohol_switch = @layout.alcohol_checkbox
+		@promille_field = @layout.promille_field
+
+		@done_button = @layout.done_button
+	end
+
+	def viewDidLoad
+		@id_field.text = 'Subject ID'
+		@alcohol_switch.on = false
+		@promille_field.text = '0'
+		@done_button.when(UIControlEventTouchUpInside) do
+			id = @id_field.text == '' ? 'No_ID' : @id_field.text
+			alcohol = @alcohol_switch.isOn
+			promille = @promille_field.text == '' ? 0 : @promille_field.text.to_f
+			subject = Subject.new(id, alcohol, promille)
+			set_subject subject
+		end
+	end
+
+
+end
