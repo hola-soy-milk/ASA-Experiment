12561ffe5e56b3139d728d788d89c3f385e82e69
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-06-25 12:19:06 +0200

    Add subject model

---
+++ app/models/subject.rb
@@ -0,0 +1,10 @@
+class Subject
+	attr_accessor :id, :alcohol?, :promille
+	
+	def initialize(id, alcohol?, promille)
+		self.id = id
+		self.alcohol? = alcohol?
+		self.promille = promille
+	end
+
+end
