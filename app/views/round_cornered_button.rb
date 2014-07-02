2b19c9a02526839280afaa8fe2495702da40c44e
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-07-02 13:20:42 +0200

    Change colors

--- app/views/round_cornered_button.rb
+++ app/views/round_cornered_button.rb
@@ -1,9 +1,18 @@
-class RoundCorneredButton < UIButton; end
+class RoundCorneredButton < UIButton
+
+  def setHighlighted(highlighted)
+    if highlighted
+      self.layer.borderColor = UIColor.lightGrayColor.CGColor
+    else
+      self.layer.borderColor = UIColor.blueColor.CGColor
+    end
+  end
+end
 
 def RoundCorneredButton.systemButton
   button = RoundCorneredButton.buttonWithType(UIButtonTypeSystem)
   button.layer.cornerRadius = 15
   button.layer.borderWidth = 2
-  button.layer.borderColor = UIColor.blackColor.CGColor
+  button.layer.borderColor = UIColor.blueColor.CGColor
   button
 end
