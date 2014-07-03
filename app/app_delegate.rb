6dbc2842a0171cc7cf4820f84df84e9875f11c8a
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-07-03 17:23:21 +0200

    Properly clone the tones

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,5 @@
 class AppDelegate
-  attr_accessor :van_hoorden_sounds, :miller_and_heise_sounds, :window, :subject, :test_count, :doing_part_2
+  attr_accessor :van_hoorden_sounds, :miller_and_heise_sounds, :window, :subject, :test_count, :doing_part_2, :time_of_test
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     self.window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
@@ -7,12 +7,13 @@ class AppDelegate
   end
 
   def start_experiment
+    self.time_of_test = Time.now.strftime("%Y%m%d_%H%M%S")
+
     self.test_count = 0
     self.doing_part_2 = false
     self.van_hoorden_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /vn/}
+    self.van_hoorden_sounds = self.van_hoorden_sounds + self.van_hoorden_sounds.dup
     self.van_hoorden_sounds.map! {|file| Tone.new(file)}
-    van_hoorden_dupe = self.van_hoorden_sounds.dup
-    self.van_hoorden_sounds = self.van_hoorden_sounds + van_hoorden_dupe
     NSLog "#{self.van_hoorden_sounds.size}"
     self.miller_and_heise_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /mh/}
     self.miller_and_heise_sounds.map! {|file| Tone.new(file)}
