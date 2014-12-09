ac44a4a5e427377ad5f58e5a36f70ebc7e986d5f
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-12-09 16:43:34 +0100

    Clean up code

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,24 +2,23 @@ class AppDelegate
   attr_accessor :van_hoorden_sounds, :miller_and_heise_sounds, :window, :subject, :test_count, :doing_part_2, :time_of_test
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    self.window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     start_experiment
   end
 
   def start_experiment
-    self.time_of_test = Time.now.strftime("%Y%m%d_%H%M%S")
+    @time_of_test = Time.now.strftime("%Y%m%d_%H%M%S")
 
-    self.test_count = 0
-    self.doing_part_2 = false
-    self.van_hoorden_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /vn/}
-    self.van_hoorden_sounds = self.van_hoorden_sounds + self.van_hoorden_sounds.dup
-    self.van_hoorden_sounds.map! {|file| Tone.new(file)}
-    NSLog "#{self.van_hoorden_sounds.size}"
-    self.miller_and_heise_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /mh/}
-    self.miller_and_heise_sounds.map! {|file| Tone.new(file)}
-    # NSLog "#{self.van_hoorden_sounds.size} #{self.miller_and_heise_sounds.size}"
-    self.window.rootViewController = TestSubjectInfoController.new
-    self.window.makeKeyAndVisible
+    @test_count = 0
+    @doing_part_2 = false
+    @van_hoorden_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /vn/}
+    @van_hoorden_sounds = @van_hoorden_sounds + @van_hoorden_sounds.dup
+    @van_hoorden_sounds.map! {|file| Tone.new(file)}
+    NSLog "#{@van_hoorden_sounds.size}"
+    @miller_and_heise_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /mh/}
+    @miller_and_heise_sounds.map! {|file| Tone.new(file)}
+    @window.rootViewController = TestSubjectInfoController.new
+    @window.makeKeyAndVisible
     true
   end
 end
