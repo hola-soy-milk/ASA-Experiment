89efd758352bca2fe3b3bf214a24a6d86ab4f755
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2014-12-09 16:48:33 +0100

    Clean up code

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -11,12 +11,12 @@ class AppDelegate
 
     @test_count = 0
     @doing_part_2 = false
-    @van_hoorden_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /vn/}
+    file_manager = NSFileManager.defaultManager
+    @van_hoorden_sounds = file_manager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select { |file| file =~ /vn/ }
     @van_hoorden_sounds = @van_hoorden_sounds + @van_hoorden_sounds.dup
-    @van_hoorden_sounds.map! {|file| Tone.new(file)}
-    NSLog "#{@van_hoorden_sounds.size}"
-    @miller_and_heise_sounds = NSFileManager.defaultManager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /mh/}
-    @miller_and_heise_sounds.map! {|file| Tone.new(file)}
+    @van_hoorden_sounds.map! { |file| Tone.new(file) }
+    @miller_and_heise_sounds = file_manager.contentsOfDirectoryAtPath(NSBundle.mainBundle.resourcePath, error:nil).select {|file| file =~ /mh/}
+    @miller_and_heise_sounds.map! { |file| Tone.new(file) }
     @window.rootViewController = TestSubjectInfoController.new
     @window.makeKeyAndVisible
     true
